1385b354178b330d4009a054bc889a60079f216c
KimDo <kaei3546@naver.com>
2019-04-10 07:24:37 +0000

    posts model add  str method

--- posts/models.py
+++ posts/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=160)
+    
+    def __str__(self):
+        return self.content
