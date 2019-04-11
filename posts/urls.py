c743cb7d18f86ebc52bf3ed2104e063037adaa40
KimDo <kaei3546@naver.com>
2019-04-11 06:11:00 +0000

    update & delete

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    
+    path('', views.index, name="index"),
     path('create/', views.create, name="create"),
+    path('<int:id>/update', views.update, name="update"),
+    path('<int:id>/delete', views.delete, name="delete")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from . forms import PostForm
 from . models import Post
+from django.views.decorators.http import require_safe
 
 # Create your views here.
 
+def index(request):
+    # Show all posts
+    posts = Post.objects.all()
+    return render(request, 'posts/index.html', {'posts': posts})
 
 def create(request):
     if request.method == "POST":
@@ -11,11 +16,30 @@ def create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('posts:create')
+            return redirect('posts:index')
         else:
             pass
         
     else: #GET
         # Show Post writing form
         form = PostForm()
-        return render(request, 'posts/create.html', {'form':form})
+        actions = 'create'
+        return render(request, 'posts/create.html', {'form':form, 'actions':actions })
+
+def update(request, id):
+    post = Post.objects.get(pk=id)
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm(instance=post)
+        actions = 'edit'
+        return render(request, 'posts/create.html', {'form': form, 'actions' : actions})
+
+@require_safe
+def delete(request, id):
+    post = Post.objects.get(pk=id)
+    post.delete()
+    return redirect('posts:index')
