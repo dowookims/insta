9c2bcaa29a78cdfa1412d037272e7f941067eb73
KimDo <kaei3546@naver.com>
2019-04-11 06:34:50 +0000

    Pillow download 및 models.py에 imageField추가, makemigrations, migrate

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=160)
+    image = models.ImageField(blank=True)
     
     def __str__(self):
         return self.content
