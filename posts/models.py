a2c6a25acdbb78fa35f514e9481c051e0be28426
KimDo <kaei3546@naver.com>
2019-04-11 08:13:33 +0000

    Model에user Field 추가

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=160)
     image = models.ImageField(blank=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.content
